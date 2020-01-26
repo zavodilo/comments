da6bf7975f05f3350f21e710bcfcd252034f79d0
Zhiganov Timofey <flashzavod@gmail.com>
2020-01-26 15:02:03 +0300

    Init new docker

--- config/db.php
+++ config/db.php
@@ -2,11 +2,17 @@
 
 return [
     'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=7781;dbname=postgres',
+    'username' => 'root',
+    'password' => 'test',
+    'charset' => 'utf8',
+    /*
+    'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
+    */
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
